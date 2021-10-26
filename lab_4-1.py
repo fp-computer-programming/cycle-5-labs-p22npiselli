34b005d7c97fa34d64bfe30778ac99e89e4464e8
p22npiselli <p22npiselli@fairfieldprep.org>
2021-10-26 10:10:09 -0400

    lab 4-1

---
+++ lab_4-1.py
@@ -0,0 +1,12 @@
+# Author: Nolan (AMDG) 10/26/2021
+
+print("Fairfield" + " Prep")
+
+print('-' * 20)
+
+previous_string = 'I\'m loving this short story I\'m reading, "The Fall of the House of Usher"'
+print(previous_string)
+print(len(previous_string))
+
+my_string = "apple"
+print(my_string[3])
