8b68331ababdb09f28537feeb95509ffeefc8fef
p22npiselli <p22npiselli@fairfieldprep.org>
2021-10-27 10:17:06 -0400

    lab 5-1 & 5-2

---
+++ lab_5-1.py
@@ -0,0 +1,9 @@
+# Author: Nolan (AMDG) 10/27/2021
+
+school = 'Fairfield Prep'
+first_half = school[:10]
+second_half = school[10:]
+print(first_half)
+print(second_half)
+new_school = (first_half + second_half)
+print(new_school)
---
+++ lab_5-2.py
@@ -0,0 +1,11 @@
+# Author: Nolan (AMDG) 10/27/2021
+
+word1 = str(input("Enter any word in lowercase: "))
+word2 = str(input("Enter another word in lowercase: "))
+ 
+if word1 < word2:
+    print(word1 + " comes before " + word2)
+elif word1 > word2:
+    print(word1 + " comes after " + word2)
+else: 
+    print(word1 + " is the same as " + word2)
