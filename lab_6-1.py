b8c7637c7d35bb2d65d9d6e04c076c511e5ee55a
p22npiselli <p22npiselli@fairfieldprep.org>
2021-10-29 10:46:51 -0400

    lab 6-1 and 6-2

---
+++ lab_6-1.py
@@ -0,0 +1,7 @@
+# Author: Nolan (AMDG) 10/29/2021
+
+x = 'flibbertigibbet'
+print(x.find("t"))
+
+b = "I wish, I wish, I was a fish."
+print(b.split(','))
---
+++ lab_6-2.py
@@ -0,0 +1,7 @@
+# Author: Nolan (AMDG) 10/29/2021
+
+user_one = input(str("User One enter your first name: "))
+user_two = input(str("User Two enter your first name: "))
+
+fs = "Hello, {0}. My name is {1}.". format(user_one, user_two)
+print(fs)
